2833f98f9806303e410db3fc73d635614ed33196
Eder <edermaxc@gmail.com>
2020-08-09 09:47:12 -0300

    impedindo de apagar o horario quando tem um unico horario disponivel

--- public/scripts/removeField.js
+++ public/scripts/removeField.js
@@ -3,6 +3,21 @@ function removeField(element){
     const scheduleList = document.querySelector("#schedule-items")
     const itemToRemove = element.parentElement.parentElement
 
-    scheduleList.removeChild(itemToRemove)
+    const nextElementSibling = itemToRemove.nextElementSibling
+    const previousElementSibling = itemToRemove.previousElementSibling
+
+    var isOnlyChield = true
+
+    if(nextElementSibling && itemToRemove.className == nextElementSibling.className){
+        isOnlyChield = false
+    }
+
+    if(previousElementSibling && itemToRemove.className == previousElementSibling.className){
+        isOnlyChield = false
+    }
+
+    if(!isOnlyChield){
+        scheduleList.removeChild(itemToRemove)
+    }
 
 }
